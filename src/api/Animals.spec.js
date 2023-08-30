970365969ca5b3374eed79575bb401fd4619fb1c
Marcelo Berte <marcelo.berte@avidity.se>
2023-08-30 17:02:14 -0300

    Changed API tests to use describe.each instead repeat all tests

--- src/api/Animals.spec.js
+++ src/api/Animals.spec.js
@@ -1,31 +1,44 @@
 const Request = require("supertest");
 const App = require('../App');
 
-const searchForAnimal = async (animal, statusCode) => {
-    const response = await Request(App).get(`/api/animals/${animal}`).expect(statusCode);
-    return expect(response.statusCode).toBe(statusCode);        
-};
+// const searchForAnimal = async (animal, statusCode) => {
+//     const response = await Request(App).get(`/api/animals/${animal}`).expect(statusCode);
+//     return expect(response.statusCode).toBe(statusCode);        
+// };
 
 // API REQUEST TESTS
 
-describe('Animals', () => {
-    it('Should return 200 fetching all animals', () => {
-        return searchForAnimal('', 200);
-    });
+// describe('Animals', () => {
+//     it('Should return 200 fetching all animals', () => {
+//         return searchForAnimal('', 200);
+//     });
 
-    it('Should return 200 searching for "Cat"', () => {
-        return searchForAnimal('cat', 200);
-    });
+//     it('Should return 200 searching for "Cat"', () => {
+//         return searchForAnimal('cat', 200);
+//     });
     
-    it('Should return 200 searching for "Dog"', () => {
-        return searchForAnimal('dog', 200);
-    });
+//     it('Should return 200 searching for "Dog"', () => {
+//         return searchForAnimal('dog', 200);
+//     });
     
-    it('Should return 200 searching for "Bird"', () => {
-        return searchForAnimal('bird', 200);
-    });
+//     it('Should return 200 searching for "Bird"', () => {
+//         return searchForAnimal('bird', 200);
+//     });
     
-    it('Should return 404 searching for "Horse"', () => {
-        return searchForAnimal('horse', 404);
+//     it('Should return 404 searching for "Horse"', () => {
+//         return searchForAnimal('horse', 404);
+//     });
+// });
+
+describe.each([
+    { query: '', expected: 200 },
+    { query: 'cat', expected: 200 },
+    { query: 'dog', expected: 200 },
+    { query: 'bird', expected: 200 },
+    { query: 'horse', expected: 404 }
+])('Animals', ({query, expected}) => {
+    it(`Should return ${expected} searching for "${query}"`, async () => {
+        const response = await Request(App).get(`/api/animals/${query}`);
+        expect(response.statusCode).toBe(expected);   
     });
 });
--- src/api/Fruits.spec.js
+++ src/api/Fruits.spec.js
@@ -1,31 +1,44 @@
 const Request = require("supertest");
 const App = require('../App');
 
-const searchForFruit = async (fruit, statusCode) => {
-    const response = await Request(App).get(`/api/fruits/${fruit}`).expect(statusCode);
-    return expect(response.statusCode).toBe(statusCode);        
-};
+// const searchForFruit = async (fruit, statusCode) => {
+//     const response = await Request(App).get(`/api/fruits/${fruit}`).expect(statusCode);
+//     return expect(response.statusCode).toBe(statusCode);        
+// };
 
 // API REQUEST TESTS
 
-describe('Fruits', () => {
-    it('Should return 200 fetching all fruits', () => {
-        return searchForFruit('', 200);
-    });
+// describe('Fruits', () => {
+//     it('Should return 200 fetching all fruits', () => {
+//         return searchForFruit('', 200);
+//     });
 
-    it('Should return 200 searching for "Apple"', () => {
-        return searchForFruit('apple', 200);
-    });
+//     it('Should return 200 searching for "Apple"', () => {
+//         return searchForFruit('apple', 200);
+//     });
     
-    it('Should return 200 searching for "Banana"', () => {
-        return searchForFruit('banana', 200);
-    });
+//     it('Should return 200 searching for "Banana"', () => {
+//         return searchForFruit('banana', 200);
+//     });
     
-    it('Should return 200 searching for "Strawberry"', () => {
-        return searchForFruit('strawberry', 200);
-    });
+//     it('Should return 200 searching for "Strawberry"', () => {
+//         return searchForFruit('strawberry', 200);
+//     });
     
-    it('Should return 404 searching for "Grape"', () => {
-        return searchForFruit('grape', 404);
+//     it('Should return 404 searching for "Grape"', () => {
+//         return searchForFruit('grape', 404);
+//     });
+// });
+
+describe.each([
+    { query: '', expected: 200 },
+    { query: 'apple', expected: 200 },
+    { query: 'banana', expected: 200 },
+    { query: 'strawberry', expected: 200 },
+    { query: 'grape', expected: 404 },
+])('Fruits', ({query, expected}) => {
+    it(`Should return ${expected} searching for "${query}"`, async () => {
+        const response = await Request(App).get(`/api/fruits/${query}`);
+        expect(response.statusCode).toBe(expected);
     });
 });
