0674835f1fc6f3cda4ae1a8079d4c4b487b0eefb
Marcelo Berte <marcelo.berte@avidity.se>
2023-09-18 17:26:16 -0300

    Refactored tests for Animals and Fruits

--- src/api/Animals.spec.js
+++ src/api/Animals.spec.js
@@ -2,34 +2,27 @@ const Request = require("supertest");
 const App = require('../App');
 
 describe.each([
-    { query: '', expected: 200 },
-    { query: 'cat', expected: 200 },
-    { query: 'dog', expected: 200 },
-    { query: 'bird', expected: 200 },
-    { query: 'horse', expected: 404 }
-])('Animals', ({ query, expected }) => {
-    it(`Should return ${expected} searching for "${query}"`, async () => {
-        const response = await Request(App).get(`/api/animals/${query}`);
-        expect(response.statusCode).toBe(expected);
-    });
-});
-
-describe.each([
+    // GET
+    { message: 'Should return 200 retrieving all Animals', query: ' ', expected: 200, method: 'get' },
+    { message: 'Should return 200 searching for "cat"', query: 'cat', expected: 200, method: 'get' },
+    { message: 'Should return 200 searching for "dog"', query: 'dog', expected: 200, method: 'get' },
+    { message: 'Should return 200 searching for "bird"', query: 'bird', expected: 200, method: 'get' },
+    { message: 'Should return 200 searching for "horse"', query: 'horse', expected: 404, method: 'get' },
+    // POST
     { message: 'Should be possible to insert a new Animal into database', payload: { name: 'Chicken' }, expected: 201 },
-    { message: 'Should not be possible to insert a new Animal if it already exists in database', payload: { name: 'Cat' }, expected: 409 }
-])('Animals', ({ message, payload, expected }) => {
+    { message: 'Should not be possible to insert a new Animal if it already exists in database', payload: { name: 'Cat' }, expected: 409 },
+    // DELETE
+    { message: 'Should be possible to delete an Animal from database', query: 'Dog', expected: 200, method: 'delete' },
+    { message: 'Should throw 404 error trying to delete an Animal that isn\'t in database', query: 'Horse', expected: 404, method: 'delete' },
+])('Animals', ({ message, payload, expected, query, method }) => {
     it(message, async () => {
-        const response = await Request(App).post('/api/animals').send(payload);
-        expect(response.statusCode).toBe(expected);
-    });
-});
-
-describe.each([
-    { message: 'Should be possible to delete an Animal from database', query: 'Dog', expected: 200 },
-    { message: 'Should throw 404 error trying to delete an Animal that isn\'t in database', query: 'Horse', expected: 404 },
-])('Animals', ({ message, query, expected }) => {
-    it(message, async () => {
-        const response = await Request(App).delete(`/api/animals/${query}`);
+        let response;
+        if (payload) {
+            response = await Request(App).post('/api/animals').send(payload);
+        }
+        if (query) {
+            response = await Request(App)[method](`/api/animals/${query}`);
+        }
         expect(response.statusCode).toBe(expected);
     });
 });
--- src/api/Fruits.spec.js
+++ src/api/Fruits.spec.js
@@ -2,34 +2,27 @@ const Request = require("supertest");
 const App = require('../App');
 
 describe.each([
-    { query: '', expected: 200 },
-    { query: 'apple', expected: 200 },
-    { query: 'banana', expected: 200 },
-    { query: 'strawberry', expected: 200 },
-    { query: 'grape', expected: 404 },
-])('Fruits', ({ query, expected }) => {
-    it(`Should return ${expected} searching for "${query}"`, async () => {
-        const response = await Request(App).get(`/api/fruits/${query}`);
-        expect(response.statusCode).toBe(expected);
-    });
-});
-
-describe.each([
+    // GET
+    { message: 'Should return 200 retrieving all Fruits', query: ' ', expected: 200, method: 'get' },
+    { message: 'Should return 200 searching for "apple"', query: 'apple', expected: 200, method: 'get' },
+    { message: 'Should return 200 searching for "banana"', query: 'banana', expected: 200, method: 'get' },
+    { message: 'Should return 200 searching for "strawberry"', query: 'strawberry', expected: 200, method: 'get' },
+    { message: 'Should return 200 searching for "grape"', query: 'grape', expected: 404, method: 'get' },
+    // POST
     { message: 'Should be possible to insert a new Fruit into database', payload: { name: 'Pear' }, expected: 201 },
-    { message: 'Should not be possible to insert a new Fruit if it already exists in database', payload: { name: 'Apple' }, expected: 409 }
-])('Fruits', ({ message, payload, expected }) => {
+    { message: 'Should not be possible to insert a new Fruit if it already exists in database', payload: { name: 'Apple' }, expected: 409 },
+    // DELETE
+    { message: 'Should be possible to delete an Fruit from database', query: 'banana', expected: 200, method: 'delete' },
+    { message: 'Should throw 404 error trying to delete an Fruit that isn\'t in database', query: 'orange', expected: 404, method: 'delete' },
+])('Fruits', ({ message, payload, expected, query, method }) => {
     it(message, async () => {
-        const response = await Request(App).post('/api/fruits').send(payload);
-        expect(response.statusCode).toBe(expected);
-    });
-});
-
-describe.each([
-    { message: 'Should be possible to delete a Fruit from database', query: 'Banana', expected: 200 },
-    { message: 'Should throw 404 error trying to delete a Fruit that isn\'t in database', query: 'Orange', expected: 404 },
-])('Fruits', ({ message, query, expected }) => {
-    it(message, async () => {
-        const response = await Request(App).delete(`/api/fruits/${query}`);
+        let response;
+        if (payload) {
+            response = await Request(App).post('/api/fruits').send(payload);
+        }
+        if (query) {
+            response = await Request(App)[method](`/api/fruits/${query}`);
+        }
         expect(response.statusCode).toBe(expected);
     });
 });
